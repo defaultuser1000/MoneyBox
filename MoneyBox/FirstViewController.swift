dc23745f2d5b032f4eda7b8cbad8c83f0e49ad8c
defaultuser1000 <and.zak.one@gmail.com>
2018-07-19 01:51:47 +0700

    First commit

--- MoneyBox/FirstViewController.swift
+++ MoneyBox/FirstViewController.swift
@@ -7,14 +7,145 @@
 //
 
 import UIKit
+import SQLite
 
-class FirstViewController: UIViewController {
+var list = ["Buy milk", "Run 5 miles", "Get Peter", "Plant my new plants"]
+var databaseGlobal: Connection!
+var goals = Table("goals")
+var id = Expression<Int>("id")
+var goalName = Expression<String>("goalName")
+var goalSum = Expression<String>("goalSum")
+var currentSum = Expression<String>("currentSum")
+var isDone = Expression<Bool>("isDone")
+var isDeleted = Expression<Bool>("isDeleted")
+var priority = Expression<Int>("priority")
+var goalsDict = [String : String]()
 
+var goalsNameList = [String]()
+var goalsSumList = [String]()
+
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var navigationBar: UINavigationBar!
+    
+    @IBOutlet weak var myTableView: UITableView!
+    
+    @IBOutlet weak var addCoinButton: UIBarButtonItem!
+    
+    @IBOutlet weak var mainListTabBarItem: UITabBarItem!
+    
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        myTableView.reloadData()
+        if goalsNameList.count > 0 {
+            addCoinButton.isEnabled = true
+            mainListTabBarItem.badgeValue = String(goalsNameList.count)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //goalsList = ["1","2","3","4"]
+        do {
+            let documentDirectory = try FileManager.default.url(for: .documentDirectory , in: .userDomainMask , appropriateFor: nil, create: true )
+            let fileUrl = documentDirectory.appendingPathComponent("GOALS").appendingPathExtension("sqlite3")
+            let database =  try Connection(fileUrl.path)
+            databaseGlobal = database
+            
+            let createTable = goals.create { (table) in
+                table.column(id, primaryKey: true)
+                table.column(goalName, unique: true)
+                table.column(goalSum)
+                table.column(currentSum)
+                table.column(isDone)
+                table.column(isDeleted)
+                table.column(priority)
+            }
+            //try database.run(goals.drop()) //Dropping table for recreation
+            try databaseGlobal.run(createTable)
+            print("Created GOALS.sqlite3" )
+            
+        } catch {
+            print(error)
+        }
+        
+        do {
+//            let query = goals.select(goalName)            SELECT "goalName" FROM "goals"
+//                        .filter(isDeleted != true)        WHERE "isDeleted" IS NOT TRUE
+            let selectedGoals = try databaseGlobal.prepare(goals.select(*).filter(isDeleted != true).order(priority))
+            
+            for goal in selectedGoals {
+                
+                goalsDict[goal[goalName]] = goal[goalSum]
+                
+                //goalsList.append(goal[goalName])
+                print("Goal ID: \(goal[id]), Goal Name: \(goal[goalName]), Goal Sum: \(goal[goalSum]), Goal Priority: \(goal[priority]), Deleted: \(goal[isDeleted])")
+            }
+        } catch {
+            print(error)
+        }
+        myTableView.reloadData()
+        if goalsNameList.count > 0 {
+            addCoinButton.isEnabled = true
+            mainListTabBarItem.badgeValue = String(goalsNameList.count)
+        }
     }
 
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return (goalsDict.count)
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
+        for (key, value) in goalsDict {
+            goalsNameList.append(key)
+            goalsSumList.append(value)
+            //cell.textLabel?.text = key + value
+        }
+        cell.textLabel?.text = goalsNameList[indexPath.row]
+        
+//        let cellIdentifier = "cell"
+//
+//        var cell : UITableViewCell
+//
+//        cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier) as! UITableViewCell
+//        cell.textLabel?.text = "text"
+//        cell.textLabel?.textAlignment = .right
+//
+//        let image : UIImage = UIImage(named: "doneAims")!
+//        print("The loaded image: \(image)")
+//        cell.imageView?.image = image
+        
+        return(cell)
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath)
+    {
+        if editingStyle == UITableViewCellEditingStyle.delete
+        {
+            let goal = goals.filter(goalName == goalsNameList[indexPath.row])
+            let deleteGoal = goal.update(isDeleted <- true);
+            do {
+                try databaseGlobal.run(deleteGoal)
+            } catch {
+                print(error)
+            }
+            goalsNameList.remove(at: indexPath.row)
+            
+            myTableView.reloadData()
+            mainListTabBarItem.badgeValue = String(Int(mainListTabBarItem.badgeValue!)! - 1)
+        }
+        if goalsNameList.count == 0 {
+            addCoinButton.isEnabled = false
+            
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -22,4 +153,5 @@ class FirstViewController: UIViewController {
 
 
 }
+
 
--- MoneyBox/SecondViewController.swift
+++ MoneyBox/SecondViewController.swift
@@ -7,9 +7,57 @@
 //
 
 import UIKit
+import SQLite
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var goalNameField: UITextField!
+    @IBOutlet weak var goalSumField: UITextField!
+    
+    var prio = [Int]()
+    
+    @IBAction func addItem(_ sender: Any) {
+        if (goalNameField.text != "" || goalSumField.text != ""){
+            //list.append(input.text!)
+            do {
+                let priorities = try databaseGlobal.prepare(goals)
+                for prior in priorities {
+                    prio.append(prior[priority])
+                }
+            } catch {
+                print(error)
+            }
+            var maxPrio = 0;
+            for tmp in prio {
+                if tmp > maxPrio {
+                    maxPrio = tmp
+                }
+            }
+            print(maxPrio)
+            let insertGoal = goals.insert(goalName <- (goalNameField.text)!, goalSum <- (goalSumField.text)!, currentSum <- "0", isDone <- false, isDeleted <- false, priority <- (maxPrio + 1))
+            goalNameField.text = ""
+            goalSumField.text = ""
+            
+            do {
+                
+                
+                
+                try databaseGlobal.run(insertGoal)
+                print("INSERTED GOAL")
+                let selectGoals = try databaseGlobal.prepare(goals)
+                for goal in selectGoals {
+                    
+                    print("Goal ID: \(goal[id]), Goal Name: \(goal[goalName]), Goal Sum: \(goal[goalSum]), Goal Priority: \(goal[priority]), Deleted: \(goal[isDeleted])")
+                    
+                }
+                
+            } catch {
+                print(error)
+            }
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
